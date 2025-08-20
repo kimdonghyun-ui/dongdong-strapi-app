853ebf36f7fcd67e3b669e0e1eb33ab6bc289a06
dongdong <dongdong@dongdongui-MacBookPro.local>
2025-08-20 16:06:53 +0900

    feat(cors): CORS 허용 도메인 수정

--- config/middlewares.js
+++ config/middlewares.js
@@ -28,13 +28,13 @@ module.exports = ({ env }) => [
         'https://www.dongdong-ui.com',
         'https://api.dongdong-ui.com', // [Strapi API 도메인]
         'http://localhost:3000', // [로컬 도메인]
-        'https://my-budget-app-steel.vercel.app', // [나만의 가계부] 
-        'https://my-word-app.vercel.app', // [영어 단어 학습]
-        'https://my-daylio-app.vercel.app', // [나만의 감정 기록]
-        'https://my-todos-app-alpha.vercel.app', // [간단한 일정등록]
-        'https://my-chat-app-lemon.vercel.app', // [간단한 채팅]
-        'https://my-food-app-kappa.vercel.app', // [맛집 찾기 앱]
-        'https://my-instagram-app-chi.vercel.app', // [인스타그램 클론 앱]
+        'https://my-budget-app.dongdong-ui.com', // [나만의 가계부] 
+        'https://my-word-app.dongdong-ui.com', // [영어 단어 학습]
+        'https://my-daylio-app.dongdong-ui.com', // [나만의 감정 기록]
+        'https://my-todos-app.dongdong-ui.com', // [간단한 일정등록]
+        'https://my-chat-app.dongdong-ui.com', // [간단한 채팅]
+        'https://my-food-app.dongdong-ui.com', // [맛집 찾기 앱]
+        'https://my-instagram-app.dongdong-ui.com', // [인스타그램 클론 앱]
       ],
     },
   },
